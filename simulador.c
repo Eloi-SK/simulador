1f9904d2455d3ef4f3c7d0e4058efc47786ef42a
Eloi Morais <eloi_matos@hotmail.com>
2018-05-08 14:45:56 -0300

    carregando hex na memoria

--- simulador.c
+++ simulador.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int getNumberLines(FILE *file);
+int getLine(FILE *file, char *buffer, size_t length);
+static int endOfLine(FILE *ifp, int c);
+
 int main(int argc, char *argv[])
 {
     if (argc < 3)
@@ -17,5 +21,54 @@ int main(int argc, char *argv[])
         printf("Erro ao carregar arquivo.");
         return -1;
     }
+
+    int lines = getNumberLines(file_in);
+    unsigned int memory[lines];
+    char line[10];
+    int i = 0;
+    fseek(file_in, 0, SEEK_SET);
+   
+    while ((getLine(file_in, line, 11)) != EOF)
+    {
+        memory[i] = (unsigned int) strtoul(line, NULL, 0);
+        i++;
+    }
+    
     return 0;
+}
+
+int getNumberLines(FILE *file)
+{
+    int lines = 0;
+    char ch;
+    while (!feof(file))
+    {
+        ch = fgetc(file);
+        if (ch == '\n')
+            lines++;
+    }
+    return lines;
+}
+
+int getLine(FILE *file, char *buffer, size_t length)
+{
+    char *end = buffer + length - 1;
+    char *dst = buffer;
+    int c;
+    while ((c = getc(file)) != EOF && !endofline(file, c) && dst < end)
+        *dst++ = c;
+    *dst = '\0';
+    return ((c == EOF && dst == buffer) ? EOF : dst - buffer);
+}
+
+static int endOfLine(FILE *ifp, int c)
+{
+    int eol = (c == '\r' || c == '\n');
+    if (c == '\r')
+    {
+        c = getc(ifp);
+        if (c != '\n' && c != EOF)
+            ungetc(c, ifp);
+    }
+    return(eol);
 }
